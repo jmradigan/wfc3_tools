68dbc3c1f2cbd7616feafca4ebf32d9a6018a59e
jmradigan <jackie.radigan@gmail.com>
2018-03-11 21:06:00 -0600

    updated axeprep and axecore scripts

--- wfc3_reduc.py
+++ wfc3_reduc.py
@@ -54,8 +54,65 @@ def wfc3_photplam():
                  "F167N": 16642.
              }
   return(photplam)
-    
-def embed_subarrays(datapath,fullframepath):
+
+def replace_mag_auto_with_plam(plam,catalog,new_catalog_name):
+  '''
+  Change the column heading in a SExtractor catalog from MAG_AUTO to
+  MAG<filter pivot wavlength> to prepare catalog for axeprep
+  This is instructed on pg 13 of the WFC3 IR grism handbook by J. Lee.
+
+  INPUTS:
+  _______
+ plam : float
+    pivot wavelength ob observations
+
+  catalog : string
+    path to catalog file
+
+  new_catalog_name : string
+    filename of new catalog to be created
+
+  '''
+  cat = open(catalog,'r')
+  outpath, fname = os.path.split(catalog)
+  outfile = os.path.join(outpath,new_catalog_name)
+  out_cat = open(outfile,'w')
+  for line in cat.readlines():
+    if 'MAG_AUTO' in line:
+      line = line.replace('MAG_AUTO','MAG_F%g'%(plam))
+    out_cat.write(line)
+  out_cat.close()
+  cat.close()
+
+def setup_environment(path):
+  dirs = {}
+  dirs['basepath'] = [path ,  "AXE_BASE_PATH"]
+  dirs['savepath'] = [os.path.join(path,"save") ,  "AXE_SAVE_PATH"]
+  dirs['datapath'] = [os.path.join(path,"DATA"), "AXE_IMAGE_PATH"]
+  dirs['fullframepath'] = [os.path.join(path,"FULLFRAME"), "AXE_FULLFRAME_PATH"]
+  dirs['imdrizzlepath'] = [os.path.join(path,"IMDRIZZLE"), "AXE_IMDRIZZLE_PATH"]
+  dirs['drizzlepath'] = [os.path.join(path,"DRIZZLE"), "AXE_OUTPUT_PATH"]
+  dirs['outputpath'] = [os.path.join(path,"OUTPUT"), "AXE_DRIZZLE_PATH"]
+  dirs['confpath'] = [os.path.join(path,"CONF"), "AXE_CONFIG_PATH"]
+  
+  for item in dirs:
+    if not os.path.exists(dirs[item][0]) :  os.makedirs(dirs[item][0])
+    os.environ[dirs[item][1]] = dirs[item][0]
+  return(dirs)
+
+def copy_data(datalocation,savepath):
+  files = glob.glob(os.path.join(datalocation,'*q_flt.fits')) + glob.glob(os.path.join(datalocation,'*q_spt.fits'))
+  for file in files:
+    try:
+      shutil.copy(file,os.path.join(savepath,os.path.split(file)[1]))
+    except OSError:
+      print('Unable to copy %s from datalocation to save/'%(file))
+      
+def parse_fits_header(fits_file, keyword):
+  hdul = fits.open(fits_file)
+  return(hdul[0].header[keyword])
+          
+def embed_subarrays(savepath,fullframepath):
   """
   Embeds subarray images in a full frame image for the aXe pipeline.
   Uses the embed_subs script from STScI.
@@ -69,15 +126,14 @@ def embed_subarrays(datapath,fullframepath):
     path where full frame data will be stored
     will be created if does not exist
   """
-  inpath = datapath
-  outpath = fullframepath
-
+  olddir = os.getcwd()
+  
   #If the fullframe path doesn't exist, then create it
   if not os.path.exists(fullframepath):
     os.makedirs(fullframepath)
-
-  #Get a list of all q _flt.fits files in the datapath
-  files = glob.glob(os.path.join(inpath,'*q_flt.fits'))
+    
+  #Get a list of all q _flt.fits files in the savepath
+  files = glob.glob(os.path.join(savepath,'*q_flt.fits'))
   
   #Copy original _flt and _spt files to fullframe directory to avoid modification
   #These files are needed for the embed_subs script
@@ -85,16 +141,20 @@ def embed_subarrays(datapath,fullframepath):
   for file in files:
     fullframe_filename=file.replace("q_flt","f_flt")
     spt_file=file.replace("_flt","_spt")
-    if not os.path.exists(os.path.join(outpath,fullframe_filename)):
-      shutil.copy(file,os.path.join(outpath,os.path.split(file)[1]))
-      shutil.copy(spt_file,os.path.join(outpath,os.path.split(spt_file)[1]))
+    if not os.path.exists(os.path.join(fullframepath,fullframe_filename)):
+      shutil.copy(file,os.path.join(fullframepath,os.path.split(file)[1]))
+      shutil.copy(spt_file,os.path.join(fullframepath,os.path.split(spt_file)[1]))
 
   #Create full frame images for all *q_flt.fits files in fullframe directory
-  subfiles = glob.glob(os.path.join(outpath,'*q_flt.fits'))
+  #Do this from the fullframepath to avoid long filesnames in fits headers
+  #(This will cause problems later on, as files name strings get cut off...)
+  os.chdir(fullframepath)
+  subfiles = glob.glob('*q_flt.fits')
   embed_subs(subfiles)        
-
+  os.chdir(olddir)
+  
   #Now go ahead and delete the copies of the original _flt and _spt files
-  copies = glob.glob(os.path.join(outpath,"*q_flt.fits")) + glob.glob(os.path.join(outpath,"*spt.fits"))
+  copies = glob.glob(os.path.join(fullframepath,"*q_flt.fits")) + glob.glob(os.path.join(fullframepath,"*spt.fits"))
   for file in copies:
     os.remove(file)
     
@@ -112,7 +172,7 @@ def filter_list(fullframepath,imdrizzlepath):
       will be created if does not exist
 
   """
-
+  
   #Get a list of all fullframe files
   files = glob.glob(os.path.join(fullframepath,"*f_flt.fits"))
 
@@ -162,10 +222,10 @@ def do_astrodrizzle(filters,imdrizzlepath):
   teal.unlearn('astrodrizzle') #teal mimics IRAF functionality, unlearn sets all parameters to default
   for filter in filters:
     if filter[0].lower() == 'f': #Check that we only have images
-      if not os.path.exists(os.path.join(imdrizzlepath,filter+'_drz.fits')):
+      if not os.path.isfile(os.path.join(imdrizzlepath,filter+'_drz.fits')):
         old_dir = os.getcwd()
         os.chdir(imdrizzlepath)  #do the stacking in the imdrizzle directory
-        astrodrizzle.AstroDrizzle(filters[filter],output=filter,build='yes',skysub=False)
+        astrodrizzle.AstroDrizzle('@%s.lis'%(filter),output=filter,build='yes',skysub=False)
         os.chdir(old_dir)  #switch back to original working directory
 
 
@@ -186,9 +246,10 @@ def do_sextractor(filters,imdrizzlepath):
   working_dir = os.getcwd()
   for filter in filters:
     if filter[0].lower() == 'f': #This is an image, not a spectrum
-
+      
       #check if catalog already exists, if not run SExtractor
-      if not os.path.isfile(os.path.join(imdrizzlepath,filter+'.cat')):
+      catalog = os.path.join(imdrizzlepath,filter+'.cat')
+      if not os.path.isfile(catalog):
 
         #SExtractor needs the SCI and WHT extensions as separate fits files
         #IRAF fails if these separate images already exist, so clean up first
@@ -203,9 +264,13 @@ def do_sextractor(filters,imdrizzlepath):
 
         #Using IRAF to copy the file extensions we want into their own images
         #(We are using IRAF instead of astropy to ensure that files are in correct format for SExtractor)
-        iraf.imcopy(os.path.join(imdrizzlepath,'%s_drz.fits[SCI]'%(filter)),os.path.join(imdrizzlepath,'%s_drz_sci.fits'%(filter)))
-        iraf.imcopy(os.path.join(imdrizzlepath,'%s_drz.fits[WHT]'%(filter)),os.path.join(imdrizzlepath,'%s_drz_wht.fits'%(filter))) 
-
+        #We are running imcopy in the imdrizzlepath to avoid long pathnames in output .fits headers
+        #(If we don't do this header info gets cut off, causing problems later)
+        os.chdir(imdrizzlepath)
+        iraf.imcopy('%s_drz.fits[SCI]'%(filter),'%s_drz_sci.fits'%(filter))
+        iraf.imcopy('%s_drz.fits[WHT]'%(filter),'%s_drz_wht.fits'%(filter)) 
+        os.chdir(working_dir)
+        
         #Look for SExtractor default files in working directory, and copy to imdrizzlepath
         default_files=['default.sex','default.param','default.conv']
         missing_default_files = [df for df in default_files if not os.path.isfile(os.path.join(imdrizzlepath,df))];
@@ -223,17 +288,13 @@ def do_sextractor(filters,imdrizzlepath):
         os.system('sex -c default.sex -WEIGHT_IMAGE %s_drz_wht.fits -MAG_ZEROPOINT %f -CATALOG_NAME %s.cat %s_drz_sci.fits' % (filter,zp,filter,filter)) 
         os.chdir(working_dir)
 
-        #Change the column heading MAG_AUTO to MAG<filter pivot wavlength> to prepare catalog for aXe
-        #This is instructed on pg 13 of the WFC3 IR grism handbook by J. Lee.
-        pw=wfc3_photplam()[filter]
-        cat = open('%s.cat'%(filter),'r')
-         out_cat = open('%s_prep.cat'%(filter),'w')
-           for line in f.readlines():
-             if 'MAG_AUTO' in line:
-               line = line.replace('MAG_AUTO','MAG_F%f'%(pw))
-             out_cat.write(line)
-          out_cat.close()
-          f.close()
+        new_catalog=os.path.join(imdrizzlepath,filter+'_prep.cat')
+        try:
+          os.remove(new_catalog)
+        except OSError:
+          pass
+        plam=wfc3_photplam()[filter]
+        replace_mag_auto_with_plam(plam,catalog,new_catalog)
         
 def identify_target(filter,imdrizzlepath):
   """
@@ -294,13 +355,9 @@ def identify_target(filter,imdrizzlepath):
 
   #add a subplot in the bottom left
   sub = fig.add_subplot(1,1,1)
-
-  #low=np.where(scl_image < 0.7)
-  #scl_image[low]=0.
   
   #populate the subplot with our image
   sub.imshow(scl_image**2,cmap='gray_r')
-  print(scl_image.min(),scl_image.max())
   
   #limit the axes to the subarray data
   sub.set_ylim([ymin,ymax])
@@ -327,80 +384,106 @@ def identify_target(filter,imdrizzlepath):
   #Create a new catalog file only containing the target for the aXe run:
   cat.meta={} #scrub the empty lines of header
   discard = [i for i, junk in enumerate(src_id) if src_id[i] != target]
-  pdb.set_trace()
   cat.remove_rows(discard)
   ascii.write(cat, os.path.join(imdrizzlepath,filter+'_prep_target.cat'))
 
-def do_axe_prepare(filters,imdrizzlepath):
-  working_dir = os.getcwd()
-  if target_name == None:
-    target_name = 'Target'
+def do_axeprep(filters,fullframepath,imdrizzlepath,basepath,datapath):
+  olddir = os.getcwd()
     
   #Get an object list for every direct image (Input Object Lists or IOL)
   #We will run this task from within the imdrizzlepath since req'd files are here
+  #The task iolprep generates an individual object catalogue for each of the direct images
   os.chdir(imdrizzlepath)
-   
   for filter in filters:
     if filter[0].lower() == 'f': #this is an image, create the IOLs
-      catname = filter+'_prep.cat'
+      catalog = filter+'_prep.cat'
       iraf.unlearn('iolprep') #unlearn sets defaults
-      iraf.iolprep(filter+'_drz.fits',catname,dimension_info='100,0,0,0',useMdriz='no')
-    working_dir = os.path.join(prog_dir,os.environ["AXE_BASE_PATH"])
-    #pdb.set_trace()
-    os.chdir(prog_dir)
-    all_files = glob.glob(os.path.join(os.environ["AXE_FULLFRAME_PATH"],"*flt.fits"))
-    i = 0
-    while i < len(all_files):
-        fits = pyfits.open(all_files[i])
-        filter = fits[0].header['FILTER']
-        fits.close()
-        if filter[0].lower() != 'f':
-            all_files.remove(all_files[i])
+      iraf.iolprep(filter+'_drz.fits',catalog,dimension_info='+100,0,0,0', useMdriz='no')
+
+  #make a list of all image files
+  all_files = glob.glob(os.path.join(fullframepath,"*flt.fits"))
+  image_files = [file for file in all_files if parse_fits_header(file, 'FILTER').lower()[0] == 'f']
+  
+  #get the MJD for each image
+  mjd_image=[]
+  for file in image_files:
+    mjd_image.append(parse_fits_header(file,'EXPSTART'))
+  mjd_array=np.array(mjd_image)
+  
+  #Associate an image file with each grism exposure
+  #Writes <grism file> <im cat file> <im file> to .lis file
+  for filter in filters:
+    if filter[0].lower() == 'g': #grism, continue
+      #open list file to write to
+      listfile = os.path.join(fullframepath,filter+".lis")
+      f = open(listfile,'w')
+      #get the MJD for each grism image
+      grism_files = filters[filter]
+      #associated_image = []
+      for file in grism_files:
+        gpath, gfile = os.path.split(file)
+        mjd = float(parse_fits_header(file,'EXPSTART'))
+        #Want the image that is closest in time before the grism exposure
+        time_diff = mjd - mjd_array
+        if True in (time_diff > 0):  #grism has image taken before it
+          ind = np.where(time_diff[time_diff > 0].min() == time_diff)
+          associated_image = image_files[ind[0]] #.append(image_files[ind[0]])
         else:
-            i = i + 1
-    for filter in filters:
-        if filter[0].lower() == 'g': #grism -- build the lists for aXe
-            f = open(os.path.join(os.environ["AXE_FULLFRAME_PATH"],filter+".lis"),'w')
-            for imgfile in filters[filter]:
-                (imgpath,imgname) = os.path.split(imgfile)
-                fits = pyfits.open(imgfile)
-                obstime = datetime.datetime.strptime(fits[0].header["DATE-OBS"]+'T'+fits[0].header["TIME-OBS"],"%Y-%m-%dT%H:%M:%S")
-                fits.close()
-                reference = None
-                overall_delta = abs(obstime - datetime.datetime.strptime("1000-01-01T00:00:00","%Y-%m-%dT%H:%M:%S"))
-                for reffile in all_files:
-                    (refpath,refname) = os.path.split(reffile)
-                    fits = pyfits.open(reffile)
-                    reftime = datetime.datetime.strptime(fits[0].header["DATE-OBS"]+'T'+fits[0].header["TIME-OBS"],"%Y-%m-%dT%H:%M:%S")
-                    fits.close()
-                    if reftime < obstime:
-                        if abs(obstime-reftime) < overall_delta:
-                            overall_delta = abs(obstime-reftime)
-                            reference = reffile
-                if reference is None:
-                    for reffile in all_files:
-                        (refpath,refname) = os.path.split(reffile)
-                        fits = pyfits.open(reffile)
-                        reftime = datetime.datetime.strptime(fits[0].header["DATE-OBS"]+'T'+fits[0].header["TIME-OBS"],"%Y-%m-%dT%H:%M:%S")
-                        fits.close()
-                        if abs(obstime-reftime) < overall_delta:
-                            overall_delta = abs(obstime-reftime)
-                            reference = reffile
-                (refpath,refname) = os.path.split(reffile)
-                f.write("%s %s %s\n" % (imgname,refname.replace("_flt.fits","_flt_1.cat"),refname))
-            f.close()
-            shutil.copy(os.path.join(os.environ["AXE_FULLFRAME_PATH"],filter+".lis"),os.path.join(os.environ["AXE_BASE_PATH"],filter+".lis"))
-            files = glob.glob(os.path.join(os.environ["AXE_FULLFRAME_PATH"],"*.fits"))
-            for file in files:
-                (p,fname) = os.path.split(file)
-                out_file = os.path.join(os.environ["AXE_IMAGE_PATH"],fname)
-                shutil.copy(file,out_file)
-            files = glob.glob(os.path.join(os.environ["AXE_IMDRIZZLE_PATH"],"i*.cat"))
-            for file in files:
-                (p,fname) = os.path.split(file)
-                out_file = os.path.join(os.environ["AXE_IMAGE_PATH"],fname)
-                shutil.copy(file,out_file)
-            iraf.unlearn('axeprep')
-            #pdb.set_trace()
-            iraf.axeprep(os.path.join(os.environ["AXE_BASE_PATH"],filter+".lis"),"WFC3.IR."+filter+".V2.5.conf",backgr='yes',backims="WFC3.IR."+filter+".sky.V1.0.fits",norm='no',mfwhm=10)
-    os.chdir(old_dir)
+          ind = np.where(time_diff.max() == time_diff)
+          associated_image = image_files[ind[0]] #.append(image_files[ind[0]])
+        ipath, ifile = os.path.split(associated_image) 
+        f.write("%s %s %s\n" % (gfile,ifile.replace("_flt.fits","_flt_1.cat"),ifile))
+      f.close()        
+
+      #copy file list into the base directory
+      shutil.copy(listfile,os.path.join(basepath,os.path.split(listfile)[1]))
+
+  #copy all _flt.fits files from FULLFRAME/ to DATA/
+  flt_files = glob.glob(os.path.join(fullframepath,'*_flt.fits'))
+  for file in flt_files:
+    shutil.copy(file,os.path.join(datapath,os.path.split(file)[1]))
+
+  #copy all _1.cat file from IMDRIZZLE/ to DATA/
+  cat_files = glob.glob(os.path.join(imdrizzlepath,'i*.cat'))
+  for file in cat_files:
+    shutil.copy(file,os.path.join(datapath,os.path.split(file)[1]))
+
+  #Run axprep: pre-processing of grism images (subtraction of a master sky)
+  #This is from from the basepath (will rely on preset environmental variables)
+  os.chdir(basepath)
+  for filter in filters:
+    if filter[0].lower() == 'g': #grism, continue
+      iraf.unlearn('axeprep')
+      listfile=filter+".lis"
+      conffile='WFC3.IR.'+filter+'.V2.5.conf'
+      background_image_files = "WFC3.IR."+filter+".sky.V1.0.fits"
+      iraf.axeprep(listfile,conffile,backgr='yes',backims=background_image_files,norm='no',mfwhm=7)
+  os.chdir(olddir)
+  
+def do_axecore(filters,basepath,back='yes',extrfwhm=4.0,drzfwhm=0.0,backfwhm=10.0,orient='no',slitless_geom='no',cont_model='gauss',sampling='drizzle',np=5,interp=1):
+  olddir = os.getcwd()
+  os.chdir(basepath)
+  for filter in filters:
+    if filter[0].lower() == 'g': #grism, continue
+      listfile = os.path.join(basepath,filter+'.lis')
+      conffile = "WFC3.IR."+filter+".V2.5.conf"
+      iraf.unlearn('axecore')
+      iraf.axecore(listfile,conffile,extrfwhm=extrfwhm,drzfwhm=drzfwhm,back=back,backfwhm=backfwhm,orient=orient,slitless_geom=slitless_geom,cont_model=cont_model,sampling=sampling,np=np, interp=interp)
+  os.chdir(olddir)   
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-p","--path",action="store",dest="path",default=os.getcwd(),metavar="PATH",help="Path for data reduction")
+    parser.add_argument("-i","--imdrizzle",action="store",dest="imdrizzle_path",default="IMDRIZZLE",help="Path to imdrizzle folder")
+    parser.add_argument("-b","--background",action="store_false",dest="background",default=True,help="Switch background subtraction in axecore")
+    results = parser.parse_args()
+
+    setup_directories(results.path)
+    embed_subarrays()
+    filters = filter_lists()
+    do_astrodrizzle(filters)
+    do_sextractor(filters)
+    do_catalogue(filters)
+    do_axe_prepare(filters)
+    do_axe_run(filters,background=results.background)
+
